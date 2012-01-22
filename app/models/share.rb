b342272196d8a1df5dcce00597602cecf12fe187
Alan Oliver <alan.j.oliver@mac.com>
2012-01-22 17:31:10 +0000

    Add validations for share model

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,2 +1,6 @@
 class Share < ActiveRecord::Base
+  attr_accessible :email, :title, :description
+
+  validates :email, :presence => true,
+                    :format   => /\A[\w+-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 end
--- spec/models/share_spec.rb
+++ spec/models/share_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Share do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation" do
+    it "should be valid with required attributes" do
+      share = Share.new(:email => "test@example.com")
+      share.should be_valid
+    end
+    it "should be invalid without an email address" do
+      share = Share.new(:email => "")
+      share.should_not be_valid
+    end
+    it "should check email validity" do
+      share = Share.new(:email => "test@example")
+      share.should_not be_valid
+    end
+  end
 end
