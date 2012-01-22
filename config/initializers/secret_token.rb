6c56723660541f3b30037139d2a78d8855f36701
Alan Oliver <alan.j.oliver@mac.com>
2012-01-22 17:21:47 +0000

    New glovebox repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glovebox::Application.config.secret_token = '41fae0eeeefb7fd5dc3b79a952b1a7e8c352e75ae6e62880a1e886773ef55c12397fc0c62050fc2114dbbae5c9522874ff538a8d5489c7641470770d416646fa'
