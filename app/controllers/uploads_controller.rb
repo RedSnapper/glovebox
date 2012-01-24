e74817927cd32836676a310c87f6240e4a17fe21
Alan Oliver <alan.j.oliver@mac.com>
2012-01-24 08:10:36 +0000

    Add :create to Uploads

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,2 +1,11 @@
 class UploadsController < ApplicationController
+  def create
+    @share = Share.find(params[:share_id])
+    @upload = @share.uploads.build(params[:upload])
+    if @upload.save
+      redirect_to share_path(params[:share_id])
+    else
+      render share_path(params[:share_id])
+    end
+  end
 end
--- spec/controllers/uploads_controller_spec.rb
+++ spec/controllers/uploads_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe UploadsController do
-
+  before(:each) do
+    @share = Share.create!(:email => "test@example.com")
+    @upload_attrs = { :name => "test" }
+  end
+  context "put :create" do
+    it "should create a new upload" do
+      lambda do
+        put :create, :share_id => @share, :upload => @upload_attrs
+      end.should change(Upload, :count).by(1)
+    end
+    it "should redirect to the share on success" do
+      put :create, :share_id => @share, :upload => @upload_attrs
+      response.should redirect_to @share
+    end
+  end
 end
