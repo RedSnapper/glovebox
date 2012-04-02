31ff6e4744e0383d3ba0342796473e759fe1286c
Alan Oliver <alanoliver@gmail.com>
2012-04-02 16:55:01 +0100

    Turn off registrations in devise

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :token_authenticatable,
+  # :token_authenticatable, :registerable, :encryptable, 
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :token_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
