ab2d669e3449e23efe71791d3a09b5cf945361d0
Alan Oliver <alanoliver@gmail.com>
2012-04-18 12:18:43 +0100

    Generate an authentication token on user creation

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,6 @@
 class Admin < ActiveRecord::Base
+  after_create :setup_user_token
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :registerable, :encryptable, 
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -7,4 +9,8 @@ class Admin < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def setup_user_token
+    self.reset_authentication_token!
+  end
 end
